a51b1d09fd65c7486148733c9e408c44aac7245c
Joseph Afful <astrolabscig@gmail.com>
2025-12-16 04:02:51 +0000

    deployment

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,7 @@ import express from 'express';
 import dotenv from 'dotenv';
 import cookieParser from 'cookie-parser';
 import cors from 'cors';
+import path from 'path';
 
 import authRoutes from './routes/auth.routes.js';
 import userRoutes from './routes/user.routes.js';
@@ -10,6 +11,8 @@ import connectToDB from './db/dbConnect.js';
 import { app, server } from './socket/socket.js'
 
 const PORT = process.env.PORT || 5000;
+const __dirname = path.resolve();
+
 dotenv.config();
 
 app.use(cors({
@@ -24,6 +27,11 @@ app.use('/api/auth', authRoutes);
 app.use('/api/messages', messageRoutes);
 app.use('/api/users', userRoutes);
 
+app.use(express.static(path.join(__dirname, "/frontend/dist")));
+
+app.get("(*)", (req, res) => {
+    res.sendFile(path.join(__dirname, "frontend", "dist", "index.html"))
+})
 
 server.listen(PORT, () => {
     connectToDB();
